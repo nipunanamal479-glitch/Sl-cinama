c943fce21a8064329519a8ddcceeea2d25914747
nipunanamal479-glitch <nipunanamal479@gmail.com>
2026-06-14 19:45:03 +0530

    Create Data.js

---
+++ Data.js
@@ -0,0 +1,14 @@
+const movies = [
+    {
+        "id": 1,
+        "title": "Yamaraja siri",
+        "image": "pI7C8jr0beI-HD.jpg",
+        "link": "https://google.com"
+    },
+    {
+        "id": 2,
+        "title": "කාක්කන් (2026)",
+        "image": "images/dridam.jpg",
+        "link": "https://google.com"
+    }
+];
